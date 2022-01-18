d047eb8ac737e6451c8fc3e83c83711b2faa37bd
avouacr <avouacr@pm.me>
2022-01-18 19:49:31 +0000

    test

---
+++ R/fonctions.R
@@ -0,0 +1,11 @@
+#' Lance l'atelier
+#'
+#' @param file le file Rmd du tutoriel
+#' @param port le port sur lequel on envoie la page web
+#' @param host l'url sur laquelle on publie
+#'
+#' @export
+launch_workshop <- function(file, port = 8181, host = 'localhost') {
+  rmarkdown::run(file = file,
+                   shiny_args = list(host = host, port = port, launch.browser = FALSE))
+}
