017c8447c52d08f5faca4cc796e4fcb219ce74f8
Romain Avouac <43444134+avouacr@users.noreply.github.com>
2022-01-19 15:15:37 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 R -e "install.packages(c('learnr'))"
 R -e "remotes::install_github('avouacr/learnr-sparkr')"
-R -e "learnr.sparkr::launch_workshop(file = system.file('sparkr-tutorial.Rmd', package = 'learnr.sparkr'), port = 8787, host = '0.0.0.0')"
+# R -e "learnr.sparkr::launch_workshop(file = system.file('sparkr-tutorial.Rmd', package = 'learnr.sparkr'), port = 8787, host = '0.0.0.0')"
